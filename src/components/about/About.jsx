64d5edc35c041d3d898a539960cb6c61fa8fd105
Leighton <leighton.l14@gmail.com>
2022-05-17 21:00:26 +1000

    Commit 4 - Added About page left side

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./about.css";
 
 const About = () => {
-  return <div className="about">About</div>;
+  //SHORTCUT: .about-right -> <div className="about-right"></div>
+  //img.about-image
+  return (
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card background"></div>
+        <div className="about-card">
+          <img
+            src="https://images-assets.nasa.gov/image/PIA15415/PIA15415~orig.jpg"
+            alt=""
+            className="about-image"
+          />
+        </div>
+      </div>
+      <div className="about-right"></div>
+    </div>
+  );
 };
 
 export default About;
