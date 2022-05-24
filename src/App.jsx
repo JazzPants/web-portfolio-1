d3761b55b7057da5437c37842133ddc67d68b7c2
Leighton <leighton.l14@gmail.com>
2022-05-24 11:23:11 +1000

    Commit 12 - Added Toggle Button component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Intro from "./components/introduction/Intro";
 import About from "./components/about/About";
 import ProjectList from "./components/projectList/ProjectList";
 import Contact from "./components/contact/Contact";
+import Toggle from "./components/Toggle/Toggle";
 
 const App = () => {
   return (
     <div>
+      <Toggle></Toggle>
       <Intro></Intro>
       <About></About>
       <ProjectList></ProjectList>
---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,17 @@
+import "./toggle.css";
+// import Sun from "../..img/sun.png"
+// import Moon from "../..img/moon.png"
+
+// {Sun}
+// {Moon}
+const Toggle = () => {
+  return (
+    <div className="toggle">
+      <img src="" alt="" className="toggle-icon" />
+      <img src="" alt="" className="toggle-icon" />
+      <div className="toggle-button"></div>
+    </div>
+  );
+};
+
+export default Toggle;
