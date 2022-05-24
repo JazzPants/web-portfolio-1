c81b0dcfb0e4f701b369100d34b1ea96da3fbb50
Leighton <leighton.l14@gmail.com>
2022-05-24 12:12:13 +1000

    Commit 14 - Toggle button works

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
 
-import React from "react";
 import Intro from "./components/introduction/Intro";
 import About from "./components/about/About";
 import ProjectList from "./components/projectList/ProjectList";
 import Contact from "./components/contact/Contact";
 import Toggle from "./components/Toggle/Toggle";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
 
 const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: darkMode ? "rgb(0, 35, 51)" : "white",
+        color: darkMode && "white",
+      }}
+    >
       <Toggle></Toggle>
       <Intro></Intro>
       <About></About>
--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context";
 import "./toggle.css";
 // import Sun from "../..img/sun.png"
 // import Moon from "../..img/moon.png"
@@ -5,11 +7,21 @@ import "./toggle.css";
 // {Sun}
 // {Moon}
 const Toggle = () => {
+  const theme = useContext(ThemeContext);
+
+  const handleClick = () => {
+    theme.dispatch({ type: "TOGGLE" });
+  };
+
   return (
     <div className="toggle">
       <img src="" alt="" className="toggle-icon" />
       <img src="" alt="" className="toggle-icon" />
-      <div className="toggle-button"></div>
+      <div
+        className="toggle-button"
+        onClick={handleClick}
+        style={{ left: theme.state.darkMode ? 0 : 25 }}
+      ></div>
     </div>
   );
 };
