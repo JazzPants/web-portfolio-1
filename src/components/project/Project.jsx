bf5bd335370c70cfedadc7806a87dcac10fb71b7
Leighton <leighton.l14@gmail.com>
2022-05-24 13:47:20 +1000

    Commit 17 - Added minor color fixes, removed some unnecessary text

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -8,7 +8,6 @@ const Project = ({ image, link }) => {
         <div className="project-circle"></div>
         <div className="project-circle"></div>
         <div className="project-circle"></div>
-        card
       </div>
       <a href="https://github.com/JazzPants" target="_blank" rel="noreferrer">
         <img src={ecommerceWebsite} alt="" className="project-image" />
